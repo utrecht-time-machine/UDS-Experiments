6f7d63e9fed076d91fc80019ef145916474cfcab
Rick Companje <rick@companje.nl>
2020-10-13 10:40:52 +0200

    rename

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# UDS-Experiments
-Experiments for Utrechts Documentatie Systeem (UDS) documentatie.org
+# Knipperendebolletjes
+door Rick, Ivar en Simon - Utrecht Time Machine
+30 maart + 13 oktober 2020
+GNU General Public License v3.0
+
+Een client-sided oplossing voor de 'knipperendebolletjes' op de zoekplaten op 
+ documentatie.org
+
--- scripts/knipperendebolletjes.js
+++ scripts/knipperendebolletjes.js
@@ -1,5 +1,38 @@
-// Source: https://stackoverflow.com/a/43747218
+/*
+knipperendebolletjes.js
+by Rick, Ivar en Simon - Utrecht Time Machine
+March 30th, 2020
+GNU General Public License v3.0
+*/
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  const queryObjNr = getParams(location.search).objectnr;
+
+  const areas = document.getElementsByTagName("area");
+  for(let i = 0; i < areas.length; i++) {
+    const area = areas.item(i);
+    const areaObjNr = getParams(area.href).Idnr;
+
+    if (queryObjNr && queryObjNr === areaObjNr && !area.hasAttribute("data-nodot")) {
+      const areaCoords = area.coords;
+      let areaPoints = coordsToPoints(areaCoords);
+      let areaCenter = get_polygon_centroid(areaPoints);
+    
+      if (area.hasAttribute("data-x")) {
+        areaCenter.x += parseInt(area.getAttribute("data-x"))
+      }
+
+      if (area.hasAttribute("data-y")) {
+        areaCenter.y += parseInt(area.getAttribute("data-y"))
+      }
+
+      createDot(areaCenter);
+    }
+  }
+});
+
 function get_polygon_centroid(pts) { // pts: { x: number, y: number }
+   //https://stackoverflow.com/a/43747218
    var first = pts[0], last = pts[pts.length-1];
    if (first.x != last.x || first.y != last.y) pts.push(first);
    var twicearea=0,
@@ -33,16 +66,16 @@ function coordsToPoints(src) {
  * @return {Object}     The URL parameters
  */
 function getParams(url) {
-	var params = {};
-	var parser = document.createElement('a');
-	parser.href = url;
-	var query = parser.search.substring(1);
-	var vars = query.split('&');
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split('=');
-		params[pair[0]] = decodeURIComponent(pair[1]);
-	}
-	return params;
+  var params = {};
+  var parser = document.createElement('a');
+  parser.href = url;
+  var query = parser.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    params[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return params;
 };
 
 function createDot(point) { // type: {x: number, y: number}
@@ -56,31 +89,3 @@ function createDot(point) { // type: {x: number, y: number}
   dot.src="/UDS4/images/Knipperbolletje.gif"
   document.body.appendChild(dot);
 };
-
-document.addEventListener("DOMContentLoaded", function(e) {
-  const queryObjNr = getParams(location.search).objectnr;
-
-  const areas = document.getElementsByTagName("area");
-  for(let i = 0; i < areas.length; i++) {
-    const area = areas.item(i);
-    const areaObjNr = getParams(area.href).Idnr;
-
-    if (queryObjNr && queryObjNr === areaObjNr && !area.hasAttribute("data-nodot")) {
-      const areaCoords = area.coords;
-      let areaPoints = coordsToPoints(areaCoords);
-      let areaCenter = get_polygon_centroid(areaPoints);
-    
-      if (area.hasAttribute("data-x")) {
-        areaCenter.x += parseInt(area.getAttribute("data-x"))
-      }
-
-      if (area.hasAttribute("data-y")) {
-        areaCenter.y += parseInt(area.getAttribute("data-y"))
-      }
-
-      createDot(areaCenter);
-    }
-  }
-});
-
-
